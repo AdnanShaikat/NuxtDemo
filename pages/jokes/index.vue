b4910114d68130e817219275d377803c8d5e3481
ShaikatHossain <shaikathossainskth@gmail.com>
2021-07-11 20:15:01 +0600

    update

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,11 +1,37 @@
 <template>
 <div>
-    <h1>Jokes</h1>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
 </div>
 </template>
 
 <script>
+import axios from "axios";
+import Joke from '@/components/Joke';
 export default {
+    components: {
+        Joke
+    },
+    data() {
+        return {
+            jokes: []
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+
+            this.jokes = res.data.results;
+        } catch (error) {
+            console.log(error)
+        }
+        
+    },
     head() {
         return {
             titile: "About The App",
